622bdb7d1f23075f91a1d6c7bcb2ca10c8bf2ffb
laissilvz <laisapdpaula@gmail.com>
2026-08-27 14:39:42 -0300

    saudação

--- js/main.js
+++ js/main.js
@@ -1,72 +1,7 @@
-const exibirSaudacao = document.querySelector("#saudacao");
- 
-const nomeCompleto = prompt("Digite seu nome completo:");
+const usuario = prompt("Digite seu nome e sobrenome");
 
-const diasSemana = [
-    "Domingo", 
-    "Segunda-feira", 
-    "Terça-feira", 
-    "Quarta-feira", 
-    "Quinta-feira", 
-    "Sexta-feira", 
-    "Sábado"
-];
+const bemVindo = document.querySelector("#saudacao");
 
-function atualizarSaudacao() {
-
-const dataAtual = new Date();
-const diaSemana = diasSemana[dataAtual.getDay()];
-const dataFormatada = dataAtual.toLocaleDateString("pt-BR", { day: "2-digit", month: "2-digit", year: "numeric" });
-const horaFormatada = dataAtual.toLocaleTimeString("pt-BR", { hour: "2-digit", minute: "2-digit" });
-const offsetMinutos = dataAtual.getTimezoneOffset();
-const offsetHoras = Math.floor(Math.abs(offsetMinutos) / 60);
-const offsetMin = Math.abs(offsetMinutos) % 60;
-const sinal = offsetMinutos > 0 ? "-" : "+";
-const fusoFormatado = `${sinal}${String(offsetHoras).padStart(2, "0")}:${String(offsetMin).padStart(2, "0")}`;
-
-const dataCompleta = `${diaSemana}, ${dataFormatada} - ${horaFormatada} (${fusoFormatado})`;
-
-exibirSaudacao.innerHTML = `Olá, ${nomeCompleto}! Hoje é ${dataCompleta}`;
-
-}
-
-atualizarSaudacao();
-
-setInterval(atualizarSaudacao,6*1000)
-
-document.addEventListener("DOMContentLoaded", () => {
-
-    const campoBusca = document.getElementById("campoBusca");
-    const linhasTabela = document.querySelectorAll("table tbody tr");
-
-    campoBusca.addEventListener("input", (event) => {
-        const termo = event.target.value.toLowerCase().trim();
-
-            linhasTabela.forEach((linha) => {
-                const texto = linha.textContent.toLowerCase();
-                linha.style.display = texto.includes(termo) ? "" : "none";
-            });
-    });
-
-    const btnTema = document.getElementById("btnTema");
-
-    btnTema.addEventListener("click", () => {
-        document.body.classList.toggle("dark-theme");
-
-        const ativo = document.body.classList.contains("dark-theme");
-        btnTema.textContent = ativo ? "Light Mode" : "Dark Mode";
-    });
-
-    const btnMenu = document.getElementById("btnMenu");
-    const mainMenu = document.getElementById("mainMenu")
-
-    btnMenu.addEventListener("click", () => {
-        mainMenu.classList.toggle("aberto");
-    });
-
-    window.addEventListener("resize", () => {
-        if (window.innerWidth > 768 && mainMenu.classList.contains("aberto")) {
-            mainMenu.classList.remove("aberto");
-        }
-    });
-});
+setInterval(() => {
+    bemVindo.innerHTML = `Olá, ${usuario}! Hoje é ${new Date().toLocaleString("pt-BR")}`;
+}, 1000);
